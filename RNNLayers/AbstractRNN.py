0f8d59c472fe74b5974f7b2ddd3192ddbd9149ad
aniltrue <anil.dogru@ozu.edu.tr>
2020-12-28 04:05:25 +0300

    LSTM is ready.

---
+++ RNNLayers/AbstractRNN.py
@@ -0,0 +1,75 @@
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import activations
+from tensorflow.keras import initializers
+from abc import ABC, abstractmethod
+
+
+class AbstractRNNCell(Layer, ABC):
+    def __init__(self,
+                 units: int,
+                 activation: str = "tanh",
+                 recurrent_activation: str = "sigmoid",
+                 kernel_initializer: str = "glorot_uniform",
+                 recurrent_initializer: str = "orthogonal",
+                 bias_initializer: str = "zeros", **kwargs):
+
+        super(AbstractRNNCell, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.recurrent_activation = activations.get(recurrent_activation)
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+    @abstractmethod
+    def call(self, inputs, states, **kwargs):
+        raise NotImplementedError("Abstract Method")
+
+
+class AbstractRNN(Layer, ABC):
+    def __init__(self, units: int, cell: AbstractRNNCell, reversed: bool = False, return_sequences: bool = False, **kwargs):
+        super(AbstractRNN, self).__init__(**kwargs)
+
+        self.units = units
+        self.cell = cell
+        self.reversed = reversed
+        self.return_sequences = return_sequences
+
+    def build(self, input_shape):
+        if isinstance(input_shape, list):
+            input_shape = input_shape[0]
+
+        if isinstance(input_shape, tf.TensorShape):
+            input_shape = tuple(input_shape.as_list())
+
+        cell_input_shape = input_shape[1:]
+
+        if isinstance(self.cell, Layer) and not self.cell.built:
+            with K.name_scope(self.cell.name):
+                self.cell.build(cell_input_shape)
+                self.cell.built = True
+
+        self.built = True
+
+    @abstractmethod
+    def get_initial_state(self, inputs):
+        pass
+
+    def call(self, inputs, **kwargs):
+        input_shape = K.int_shape(inputs)
+
+        def step_fn(inputs, states):
+            output, new_states = self.cell.call(inputs, states, **kwargs)
+
+            return output, new_states
+
+        initial_states = self.get_initial_state(inputs),
+
+        last_output, outputs, states = K.rnn(step_fn, inputs, initial_states=initial_states, go_backwards=self.reversed, input_length=input_shape[1])
+
+        if self.return_sequences:
+            return outputs
+        else:
+            return last_output
---
+++ RNNLayers/LSTM.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from RNNLayers.AbstractRNN import AbstractRNNCell, AbstractRNN
+import tensorflow.keras.backend as K
+
+
+class LSTMCell(AbstractRNNCell):
+    def __init__(self,
+                 units: int,
+                 activation: str = "tanh",
+                 recurrent_activation: str = "sigmoid",
+                 kernel_initializer: str = "glorot_uniform",
+                 recurrent_initializer: str = "orthogonal",
+                 bias_initializer: str = "zeros", **kwargs):
+
+        super(LSTMCell, self).__init__(units, activation, recurrent_activation,
+                                       kernel_initializer, recurrent_initializer, bias_initializer, **kwargs)
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+
+        # Input Gate
+        self.ki = self.add_weight("kernel_input", (input_dim, self.units), initializer=self.kernel_initializer)
+        self.ri = self.add_weight("recurrent_input", (self.units, self.units), initializer=self.recurrent_initializer)
+        self.bi = self.add_weight("bias_input", (self.units, ), initializer=self.bias_initializer)
+
+        # Forget Gate
+        self.kf = self.add_weight("kernel_forget", (input_dim, self.units), initializer=self.kernel_initializer)
+        self.rf = self.add_weight("recurrent_forget", (self.units, self.units), initializer=self.recurrent_initializer)
+        self.bf = self.add_weight("bias_forget", (self.units,), initializer=self.bias_initializer)
+
+        # Output Gate
+        self.ko = self.add_weight("kernel_output", (input_dim, self.units), initializer=self.kernel_initializer)
+        self.ro = self.add_weight("recurrent_output", (self.units, self.units), initializer=self.recurrent_initializer)
+        self.bo = self.add_weight("bias_output", (self.units,), initializer=self.bias_initializer)
+
+        # Cell Gate
+        self.kc = self.add_weight("kernel_cell", (input_dim, self.units), initializer=self.kernel_initializer)
+        self.rc = self.add_weight("recurrent_cell", (self.units, self.units), initializer=self.recurrent_initializer)
+        self.bc = self.add_weight("bias_cell", (self.units,), initializer=self.bias_initializer)
+
+    def call(self, inputs, states, **kwargs):
+        h_prev = states[0][0]
+        c_prev = states[0][1]
+
+        _input = self.recurrent_activation(K.dot(inputs, self.ki) + K.dot(h_prev, self.ri) + self.bi)
+        _forget = self.recurrent_activation(K.dot(inputs, self.kf) + K.dot(h_prev, self.rf) + self.bf)
+        _output = self.recurrent_activation(K.dot(inputs, self.ko) + K.dot(h_prev, self.ro) + self.bo)
+
+        _c = self.activation(K.dot(inputs, self.kc) + K.dot(h_prev, self.rc))
+
+        c = self.recurrent_activation(_forget * c_prev + _input * _c)
+        h = self.activation(c) * _output
+
+        return h, [h, c]
+
+
+class LSTM(AbstractRNN):
+    def __init__(self,
+                 units: int,
+                 activation: str = "tanh",
+                 recurrent_activation: str = "sigmoid",
+                 kernel_initializer: str = "glorot_uniform",
+                 recurrent_initializer: str = "orthogonal",
+                 bias_initializer: str = "zeros",
+                 reversed: bool = False,
+                 return_sequences: bool = False,
+                 **kwargs):
+
+        cell = LSTMCell(units, activation, recurrent_activation,
+                        kernel_initializer, recurrent_initializer, bias_initializer, **kwargs)
+
+        super(LSTM, self).__init__(units, cell, reversed, return_sequences, **kwargs)
+
+    def get_initial_state(self, inputs):
+        batch_size = tf.compat.v1.shape(inputs)[0]
+        dtype = inputs.dtype
+
+        flat_dims = tf.TensorShape([self.units]).as_list()
+        init_state_size = [batch_size] + flat_dims
+
+        return tf.zeros(init_state_size, dtype=dtype), tf.zeros(init_state_size, dtype=dtype)
