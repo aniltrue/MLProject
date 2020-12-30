dd0af14696a0cd97a82c49970c84f82bfb06edac
aniltrue <anil.dogru@ozu.edu.tr>
2020-12-30 16:55:28 +0300

    LSTM with Peephole connection is added.

--- RNNLayers/LSTM.py
+++ RNNLayers/LSTM.py
@@ -1,33 +1,43 @@
-import tensorflow as tf
 from RNNLayers.RNNBuilder import RNNCellBuilder, AbstractRNNBuilder, AbstractBiRNNBuilder
-import tensorflow.keras.backend as K
 
 
 def lstm_cell(units: int,
-             kernel_activation: str = "tanh",
-             recurrent_activation: str = "sigmoid",
-             kernel_initializer: str = "glorot_uniform",
-             recurrent_initializer: str = "orthogonal",
-             bias_initializer: str = "zeros",
-             use_bias: bool = True,
-             **kwargs) -> RNNCellBuilder:
+              peephole: bool = False,
+              kernel_activation: str = "tanh",
+              recurrent_activation: str = "sigmoid",
+              kernel_initializer: str = "glorot_uniform",
+              recurrent_initializer: str = "orthogonal",
+              bias_initializer: str = "zeros",
+              use_bias: bool = True,
+              **kwargs) -> RNNCellBuilder:
 
     cell = RNNCellBuilder(units, ["h", "C"], kernel_activation, recurrent_activation,
                           kernel_initializer, recurrent_initializer, bias_initializer, use_bias, **kwargs)
 
-    return cell\
-        .add_recurrent("input", ["X", "h"])\
-        .add_recurrent("forget", ["X", "h"])\
-        .add_recurrent("output", ["X", "h"])\
-        .add_kernel("cell", ["X", "h"])\
+    if peephole:
+        return cell \
+            .add_recurrent("input", ["X", "h", "C"]) \
+            .add_recurrent("forget", ["X", "h", "C"]) \
+            .add_recurrent("output", ["X", "h", "C"]) \
+            .add_kernel("cell", ["X", "h"]) \
+            .add_var("C_next", ["forget", "C", "input", "cell"],
+                     lambda inps: cell.recurrent_activation(inps[0] * inps[1] + inps[2] * inps[3])) \
+            .add_var("h_next", ["C_next", "output"], lambda inps: cell.kernel_activation(inps[0]) * inps[1])
+
+    return cell \
+        .add_recurrent("input", ["X", "h"]) \
+        .add_recurrent("forget", ["X", "h"]) \
+        .add_recurrent("output", ["X", "h"]) \
+        .add_kernel("cell", ["X", "h"]) \
         .add_var("C_next", ["forget", "C", "input", "cell"],
-                 lambda X: cell.recurrent_activation(X[0] * X[1] + X[2] * X[3]))\
+                 lambda X: cell.recurrent_activation(X[0] * X[1] + X[2] * X[3])) \
         .add_var("h_next", ["C_next", "output"], lambda X: cell.kernel_activation(X[0]) * X[1])
 
 
 class LSTM(AbstractRNNBuilder):
     def __init__(self,
                  units: int,
+                 peephole: bool,
                  kernel_activation: str = "tanh",
                  recurrent_activation: str = "sigmoid",
                  kernel_initializer: str = "glorot_uniform",
@@ -38,9 +48,11 @@ class LSTM(AbstractRNNBuilder):
                  use_bias: bool = True,
                  **kwargs):
 
+        self.peephole = peephole
+
         super(LSTM, self).__init__(units, kernel_activation, recurrent_activation,
-                                  kernel_initializer, recurrent_initializer, bias_initializer,
-                                  reversed, return_sequences, use_bias, **kwargs)
+                                   kernel_initializer, recurrent_initializer, bias_initializer,
+                                   reversed, return_sequences, use_bias, **kwargs)
 
     def get_cell(self, units: int,
                  kernel_activation: str,
@@ -51,14 +63,16 @@ class LSTM(AbstractRNNBuilder):
                  use_bias: bool,
                  **kwargs) -> RNNCellBuilder:
 
-        return lstm_cell(units, kernel_activation, recurrent_activation,
-                        kernel_initializer, recurrent_initializer, bias_initializer,
-                        use_bias, **kwargs)
+        return lstm_cell(units, self.peephole,
+                         kernel_activation, recurrent_activation,
+                         kernel_initializer, recurrent_initializer, bias_initializer,
+                         use_bias, **kwargs)
 
 
 class BiLSTM(AbstractBiRNNBuilder):
     def __init__(self,
                  units: int,
+                 peephole: bool = False,
                  kernel_activation: str = "tanh",
                  recurrent_activation: str = "sigmoid",
                  kernel_initializer: str = "glorot_uniform",
@@ -68,24 +82,13 @@ class BiLSTM(AbstractBiRNNBuilder):
                  use_bias: bool = True,
                  **kwargs):
 
+        self.peephole = peephole
+
         super(BiLSTM, self).__init__(units, kernel_activation, recurrent_activation,
-                                   kernel_initializer, recurrent_initializer, bias_initializer,
-                                   return_sequences, use_bias, **kwargs)
+                                     kernel_initializer, recurrent_initializer, bias_initializer,
+                                     return_sequences, use_bias, **kwargs)
 
     def get_cell_forward(self, units: int,
-                 kernel_activation: str,
-                 recurrent_activation: str,
-                 kernel_initializer: str,
-                 recurrent_initializer: str,
-                 bias_initializer: str,
-                 use_bias: bool,
-                 **kwargs) -> RNNCellBuilder:
-
-        return lstm_cell(units, kernel_activation, recurrent_activation,
-                         kernel_initializer, recurrent_initializer, bias_initializer,
-                         use_bias, **kwargs)
-
-    def get_cell_backward(self, units: int,
                          kernel_activation: str,
                          recurrent_activation: str,
                          kernel_initializer: str,
@@ -94,6 +97,21 @@ class BiLSTM(AbstractBiRNNBuilder):
                          use_bias: bool,
                          **kwargs) -> RNNCellBuilder:
 
-        return lstm_cell(units, kernel_activation, recurrent_activation,
+        return lstm_cell(units, self.peephole,
+                         kernel_activation, recurrent_activation,
+                         kernel_initializer, recurrent_initializer, bias_initializer,
+                         use_bias, **kwargs)
+
+    def get_cell_backward(self, units: int,
+                          kernel_activation: str,
+                          recurrent_activation: str,
+                          kernel_initializer: str,
+                          recurrent_initializer: str,
+                          bias_initializer: str,
+                          use_bias: bool,
+                          **kwargs) -> RNNCellBuilder:
+
+        return lstm_cell(units, self.peephole,
+                         kernel_activation, recurrent_activation,
                          kernel_initializer, recurrent_initializer, bias_initializer,
                          use_bias, **kwargs)
