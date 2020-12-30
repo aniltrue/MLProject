37afda92ffb9fd31f06ece0db8da0f47be3ed39d
aniltrue <anil.dogru@ozu.edu.tr>
2020-12-30 23:08:08 +0300

    LSTM Variants: LSTM without Forget Gate is implemented.

--- RNNLayers/LSTMVariants.py
+++ RNNLayers/LSTMVariants.py
@@ -11,7 +11,6 @@ def lstm_nig_cell(units: int,
                   bias_initializer: str = "zeros",
                   use_bias: bool = True,
                   **kwargs) -> RNNCellBuilder:
-
     cell = RNNCellBuilder(units, ["h", "C"], kernel_activation, recurrent_activation,
                           kernel_initializer, recurrent_initializer, bias_initializer, use_bias, **kwargs)
 
@@ -31,6 +30,39 @@ def lstm_nig_cell(units: int,
         .add_kernel("cell", ["X", "h"]) \
         .add_var("C_next", ["forget", "C", "cell"],
                  lambda x: cell.recurrent_activation(x[0] * x[1] + x[2])) \
+        .add_var("h_next", ["C_next", "output"], lambda x: cell.kernel_activation(x[0]) * x[1])
+
+
+# LSTM without Forget Gate
+def lstm_nfg_cell(units: int,
+                  peephole: bool = False,
+                  kernel_activation: str = "tanh",
+                  recurrent_activation: str = "hard_sigmoid",
+                  kernel_initializer: str = "glorot_uniform",
+                  recurrent_initializer: str = "orthogonal",
+                  bias_initializer: str = "zeros",
+                  use_bias: bool = True,
+                  **kwargs) -> RNNCellBuilder:
+
+    cell = RNNCellBuilder(units, ["h", "C"], kernel_activation, recurrent_activation,
+                          kernel_initializer, recurrent_initializer, bias_initializer, use_bias, **kwargs)
+
+    # forget = 1
+    if peephole:
+        return cell \
+            .add_recurrent("input", ["X", "h", "C"]) \
+            .add_recurrent("output", ["X", "h", "C"]) \
+            .add_kernel("cell", ["X", "h"]) \
+            .add_var("C_next", ["C", "input", "cell"],
+                     lambda x: cell.recurrent_activation(x[0] + x[1] * x[2])) \
+            .add_var("h_next", ["C_next", "output"], lambda x: cell.kernel_activation(x[0]) * x[1])
+
+    return cell \
+        .add_recurrent("input", ["X", "h"]) \
+        .add_recurrent("output", ["X", "h"]) \
+        .add_kernel("cell", ["X", "h"]) \
+        .add_var("C_next", ["C", "input", "cell"],
+                 lambda x: cell.recurrent_activation(x[0] + x[1] * x[2])) \
         .add_var("h_next", ["C_next", "output"], lambda x: cell.kernel_activation(x[0]) * x[1])
 
 
