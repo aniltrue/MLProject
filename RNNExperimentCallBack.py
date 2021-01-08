a5aebe48936fe03dddf2ad64b633857f3bff34b0
aniltrue <anil.dogru@ozu.edu.tr>
2021-01-08 14:49:42 +0300

    Evaluation Logging Feature is added.

--- RNNExperimentCallBack.py
+++ RNNExperimentCallBack.py
@@ -7,12 +7,15 @@ import matplotlib.pyplot as plt
 
 
 class RNNExperimentCallBack(Callback):
-    def __init__(self, model: Model, dataset: str, log_dir: str = "logs/", experiments_path: str = "experiments.csv"):
+    def __init__(self, model: Model, dataset: str, metric: str = "acc",
+                 log_dir: str = "logs/", experiments_path: str = "experiments.csv"):
+
         super().__init__()
 
         self.log_dir = log_dir
         self.experiments_path = experiments_path
         self.model = model
+        self.metric = metric
 
         self.experiments = pd.read_csv(experiments_path)
 
@@ -51,10 +54,13 @@ class RNNExperimentCallBack(Callback):
 
         self.format = "%s_%s_%d_%d_%s" % (experiment_name, dataset, experiment_id, layer_size, layer_activation)
 
-        self.logs = pd.DataFrame(columns=["epoch", "loss", "acc", "val_loss", "val_acc"])
+        self.logs = pd.DataFrame(columns=["epoch", "loss", self.metric, "val_loss", "val_%s" % self.metric])
 
     def on_train_begin(self, logs=None):
         self.logs.to_csv(os.path.join(self.log_dir, "%s.csv" % self.format))
+        print("-" * 50)
+        print("Experiment %d(%s) training begins." % (self.id, self.name))
+        print("-" * 50)
 
     def on_train_end(self, logs=None):
         self.model.save_weights(os.path.join(self.log_dir, "%s.h5" % self.format))
@@ -62,9 +68,9 @@ class RNNExperimentCallBack(Callback):
         epochs = self.logs.shape[0]
 
         self.experiments.loc[self.experiments["ID"] == self.id, ["loss"]] = logs["loss"]
-        self.experiments.loc[self.experiments["ID"] == self.id, ["acc"]] = logs["acc"]
+        self.experiments.loc[self.experiments["ID"] == self.id, ["metric"]] = logs[self.metric]
         self.experiments.loc[self.experiments["ID"] == self.id, ["val_loss"]] = logs["val_loss"]
-        self.experiments.loc[self.experiments["ID"] == self.id, ["val_acc"]] = logs["val_acc"]
+        self.experiments.loc[self.experiments["ID"] == self.id, ["val_metric"]] = logs["val_%s" % self.metric]
         self.experiments.loc[self.experiments["ID"] == self.id, ["Epoch"]] = epochs
 
         self.experiments.to_csv(self.experiments_path)
@@ -73,14 +79,14 @@ class RNNExperimentCallBack(Callback):
 
         axs[0].plot(list(range(1, epochs + 1)), self.logs["loss"].to_numpy(), c="r", label="train")
         axs[0].plot(list(range(1, epochs + 1)), self.logs["val_loss"].to_numpy(), c="b", label="test")
-        axs[1].plot(list(range(1, epochs + 1)), self.logs["acc"].to_numpy(), c="r", label="train")
-        axs[1].plot(list(range(1, epochs + 1)), self.logs["val_acc"].to_numpy(), c="b", label="test")
+        axs[1].plot(list(range(1, epochs + 1)), self.logs["metric"].to_numpy(), c="r", label="train")
+        axs[1].plot(list(range(1, epochs + 1)), self.logs["val_metric"].to_numpy(), c="b", label="test")
 
         axs[0].set_title("Loss for %s" % self.name)
         axs[0].legend()
         axs[0].set_xticks(list(range(1, epochs + 1)))
 
-        axs[1].set_title("Accuracy for %s" % self.name)
+        axs[1].set_title("Metric (%s) for %s" % (self.metric, self.name))
         axs[1].legend()
         axs[1].set_xticks(list(range(1, epochs + 1)))
 
@@ -93,17 +99,44 @@ class RNNExperimentCallBack(Callback):
         print("-" * 50)
         print("Experiment %d(%s) is completed." % (self.id, self.name))
         print("Training Loss:\t{:.3f}".format(logs["loss"]))
-        print("Test Loss:\t{:.3f}".format(logs["val_loss"]))
-        print("Training Accuracy:\t{:.2f}%".format(logs["acc"] * 100))
-        print("Test Accuracy:\t{:.2f}%".format(logs["val_acc"] * 100))
+        print("Validation Loss:\t{:.3f}".format(logs["val_loss"]))
+
+        if self.metric == "acc" or self.metric == "accuracy":
+            print("Training Accuracy:\t{:.2f}%".format(logs[self.metric] * 100))
+            print("Validation Accuracy:\t{:.2f}%".format(logs["val_%s" % self.metric] * 100))
+        else:
+            print("Training {}:\t{:.4f}%".format(self.metric, logs[self.metric]))
+            print("Validation {}:\t{:.4f}%".format(self.metric, logs["val_%s" % self.metric]))
+
+        print("-" * 50)
+
+    def on_test_begin(self, logs=None):
+        print("-" * 50)
+        print("Experiment %d(%s) test begins." % (self.id, self.name))
+        print("-" * 50)
+
+    def on_test_end(self, logs=None):
+        self.experiments.loc[self.experiments["ID"] == self.id, ["test_loss"]] = logs["loss"]
+        self.experiments.loc[self.experiments["ID"] == self.id, ["test_metric"]] = logs[self.metric]
+
+        self.experiments.to_csv(self.experiments_path)
+
+        print("-" * 50)
+        print("Experiment %d(%s) test is completed." % (self.id, self.name))
+        print("Test Loss:\t{:.3f}".format(logs["loss"]))
+        if self.metric == "acc" or self.metric == "accuracy":
+            print("Test Accuracy:\t{:.2f}".format(logs[self.metric] * 100))
+        else:
+            print("Test {}:\t{:.4f}".format(self.metric, logs[self.metric]))
+
         print("-" * 50)
 
     def on_epoch_end(self, epoch, logs=None):
         row = {"epoch": epoch,
                "loss": logs["loss"],
-               "acc": logs["acc"],
+               self.metric: logs[self.metric],
                "val_loss": logs["val_loss"],
-               "val_acc": logs["val_acc"]}
+               "val_%s" % self.metric: logs["val_%s" % self.metric]}
 
         self.logs = self.logs.append(row, ignore_index=True)
 
